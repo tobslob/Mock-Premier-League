3dd0815eaf5afac40edd734de4eb2c46a320dd89
tobslob <kazmobileapp@gmail.com>
2019-09-24 17:38:38 +0100

    feat(Edit-Team): Admin can edit team
    - Create a edit controller
    - Create a edit route
    - Update utils messages
    
    [Finishes]

--- src/controllers/TeamController.js
+++ src/controllers/TeamController.js
@@ -91,6 +91,59 @@ class TeamController {
         });
     }
   }
+
+  /**
+   *  admin can edit a team method
+   * @param {object} req - response object
+   * @param {object} res - response object
+   * @param {number} status - http status code
+   * @param {string} statusMessage - http status message
+   * @param {object} data - response data
+   *
+   * @returns {object} returns response
+   *
+   * @example
+   *
+   */
+  static async editTeam(req, res) {
+    const { teamName, teamMembers, description } = req.body;
+    try {
+      const team = await TeamModel.findByIdAndUpdate(
+        { _id: req.params.teamId },
+        {
+          $set: {
+            teamName,
+            teamMembers,
+            description,
+            updatedAt: Date.now()
+          }
+        },
+        { useFindAndModify: false }
+      )
+        .exec();
+      if (!req.user.isAdmin) {
+        return response(res, 403, 'error', {
+          message: messages.unAuthorizedRoute
+        });
+      }
+      if (!team) {
+        return response(res, 404, 'error', {
+          message: messages.notfound
+        });
+      }
+      return response(res, 200, 'success', {
+        message: messages.updateMessage
+      });
+    } catch (error) {
+      error.name === 'CastError'
+        ? response(res, 400, 'error', {
+          message: messages.castError
+        })
+        : response(res, 400, 'error', {
+          message: messages.error
+        });
+    }
+  }
 }
 
 export default TeamController;
